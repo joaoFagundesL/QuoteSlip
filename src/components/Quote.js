5eed2119a228cfc38facb60940d8652cbe0ba625
joao <joao.230702@alunos.utfpr.edu.br>
2023-02-07 14:08:03 -0300

    att

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import {useEffect, useState} from 'react';
+import './_quote.scss';
 
 const Quote = () => {
-  const [quote, setQuote] = useState([]);
+  const [quote, setQuote] = useState({ content: '', author: ''});
 
-  const loadQuote = () => {
-    fetch('https://api.quotable.io/random')
-      .then(response => response.json())
-      .then(data => setQuote(data.content))
-      .catch(error => console.error(error));
+  const loadQuote = async () => {
+    try {
+      const response = await fetch ('https://api.quotable.io/random');
+      const data = await response.json();
+      setQuote( {
+          quote: data.content,
+          author: data.author,
+        });
+    } catch (e) {
+      console.error(e);
+    }
   }
 
   useEffect(() => {
@@ -16,9 +23,10 @@ const Quote = () => {
   }, [])
 
   return(
-    <div>
-      <h1>{quote}</h1>
-      <button onClick={ () => loadQuote() }>Click</button>
+    <div className='quote-container'>
+      <p className='quote-author'>{quote.author}</p>
+      <h1 className='quote-phrase'>{quote.quote}</h1>
+      <button className='quote-button' onClick={ () => loadQuote() }>Click</button>
     </div>
   );
 };
